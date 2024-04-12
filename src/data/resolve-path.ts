7529eb15588bbe076b094030a57fa530a9aefe68
LucasLessaAnacleto <lucaslessaanacleto@gmail.com>
2024-04-12 15:30:29 -0300

    resolve path

--- src/data/resolve-path.ts
+++ src/data/resolve-path.ts
@@ -3,7 +3,7 @@ const url = new URL(window.location.toString());
 const config: {path_name_root?: string} = {
     path_name_root: url.host === "localhost:5173" ? 
     "" : // raiz
-    "" // define aqui um sub-diretório, caso a sua aplicação não esteja na raiz do servidor
+    "/twitter-ui" // define aqui um sub-diretório, caso a sua aplicação não esteja na raiz do servidor
 }
 
 export default function resolvePath(path: string) : string {
